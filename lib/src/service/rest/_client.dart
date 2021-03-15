a588ec33fc7aaad9001cec5c6c1852525c1b5e3c
arish <arishkhan28.08@gmail.com>
2021-03-15 14:49:46 +0500

    Added test-Release Url

--- lib/src/service/rest/_client.dart
+++ lib/src/service/rest/_client.dart
@@ -3,6 +3,6 @@ import 'package:flutter/foundation.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 final dioClient = Dio(BaseOptions(
-  baseUrl: kDebugMode ? 'http://192.168.1.8:4000' : ''
+  baseUrl: kDebugMode ? 'http://192.168.1.8:4000' : 'https://mysterious-garden-78033.herokuapp.com'
 ))
 ..interceptors.add(PrettyDioLogger(requestBody: true));

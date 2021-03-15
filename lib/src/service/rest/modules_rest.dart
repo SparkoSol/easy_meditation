5a8ed9c4da74e12cab3261aeb4de0bf88157fb58
Rashid-khabeer <rkhabeer84@gmail.com>
2021-03-15 17:33:26 +0500

    removed retrofit

--- lib/src/service/rest/modules_rest.dart
+++ lib/src/service/rest/modules_rest.dart
@@ -1,14 +1,15 @@
 import 'package:easy_meditation/src/models/module.dart';
 import 'package:easy_meditation/src/service/rest/_client.dart';
-import 'package:retrofit/http.dart';
+
+// import 'package:retrofit/http.dart';
 import 'package:dio/dio.dart';
 
 part 'modules_rest.g.dart';
 
-@RestApi()
+// @RestApi()
 abstract class ModulesRestAPI {
   factory ModulesRestAPI() => _ModulesRestAPI(dioClient);
 
-  @GET('/courses/{id}')
-  Future<List<Module>> getModules(@Path('id') int number);
+  // @GET('/courses/{id}')
+  Future<List<Module>> getModules(int number) async => [];
 }
